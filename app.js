ba93ce2523a7b39a29cba140c5a1a3659ba63731
yaduit <yk226411@gmail.com>
2025-11-28 10:01:13 +0530

    created user and product models

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
+const path = require('path');
+app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cookieParser());
 
 app.get('/',function(req,res){
     res.send("hello dear");
@@ -8,4 +17,4 @@ app.get('/',function(req,res){
 
 app.listen(3000,function(){
     console.log("server is running on port 3000");
-})
+});
